7ef103b6399834fb980276c155568616f397010d
Renato <renatoestuda316@gmail.com>
2025-05-07 16:59:55 -0300

    Update siteendeota.py

--- siteendeota.py
+++ siteendeota.py
@@ -15,7 +15,7 @@ import secrets
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'amerdadakey'
+app.secret_key = 'senhamuitosecreta1234'
 ph = PasswordHasher()
 
 AES_KEY = os.getenv('AES_KEY').encode()  
